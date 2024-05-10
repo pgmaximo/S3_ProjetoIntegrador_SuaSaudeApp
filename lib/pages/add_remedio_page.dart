a7bc209c5dced1dd00f70a32e0bcb888a571aaed
Arturo <arturoochoagarcia.aog@gmail.com>
2024-05-10 16:27:18 -0300

    correcoes add_remedio_page

--- lib/pages/add_remedio_page.dart
+++ lib/pages/add_remedio_page.dart
@@ -14,48 +14,44 @@ class _AddRemedioPageState extends State<AddRemedioPage> {
   final MedicamentoService medicamentoService = MedicamentoService();
   String? medicamentoSelec;
   List<String> nomeMedicamento = [];
+  List<String> filteredNomeMedicamento = [];
+  TextEditingController searchController = TextEditingController();
 
-  // Generate dynamic options
-  List<String> generateTimes() {
-    List<String> times = [];
-    for (int hour = 0; hour < 24; hour++) {
-      for (int min = 0; min < 60; min += 30) {
-        String timeString = '${hour.toString().padLeft(2, '0')}:${min.toString().padLeft(2, '0')}';
-        times.add(timeString);
-      }
-    }
-    return times;
-  }
+  List<String> times = List.generate(48, (index) => "${index ~/ 2}:${(index % 2 * 30).toString().padLeft(2, '0')}");
+  List<String> periods = List.generate(30, (index) => '${index + 1} dias');
+  List<String> intervals = List.generate(24, (index) => '${index + 1}h');
 
-  List<String> generatePeriods(int days) {
-    return List.generate(days, (index) => '${index + 1} dias');
-  }
-
-  List<String> generateIntervals(int hours) {
-    return List.generate(hours, (index) => '${index + 1}h');
-  }
-
-  String? horarioSelec;
-  String? periodoSelec;
-  String? intervaloSelec;
-
-  List<String> horarios = [];
-  List<String> periodos = [];
-  List<String> intervalos = [];
+  String? selectedTime;
+  String? selectedPeriod;
+  String? selectedInterval;
 
   @override
   void initState() {
     super.initState();
-    horarios = generateTimes();
-    periodos = generatePeriods(365); 
-    intervalos = generateIntervals(48); 
-
     medicamentoService.getMedicationNames().listen((names) {
       setState(() {
         nomeMedicamento = names;
+        filteredNomeMedicamento = names;
+        if (nomeMedicamento.isNotEmpty) {
+          medicamentoSelec = nomeMedicamento.first;
+        }
       });
     }, onError: (error) {
       print("Ocorreu um erro ao carregar os nomes dos medicamentos: $error");
+    });
+
+    searchController.addListener(() {
+      filterSearchResults(searchController.text);
+    });
+  }
+
+  void filterSearchResults(String query) {
+    List<String> dummyListData = nomeMedicamento
+        .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    setState(() {
+      filteredNomeMedicamento = dummyListData;
+      medicamentoSelec = dummyListData.contains(medicamentoSelec) ? medicamentoSelec : null;
     });
   }
 
@@ -63,94 +59,107 @@ class _AddRemedioPageState extends State<AddRemedioPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const AppBarWidget(titulo: 'Adicionar Medicamento', logout: false),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            if (nomeMedicamento.isNotEmpty) ...[
-              DropdownButton<String>(
-                value: medicamentoSelec,
-                hint: const Text("Selecione um Medicamento"),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    medicamentoSelec = newValue;
-                  });
-                },
-                items: nomeMedicamento.map<DropdownMenuItem<String>>((String name) {
-                  return DropdownMenuItem<String>(
-                    value: name,
-                    child: Text(name),
-                  );
-                }).toList(),
-              ),
-              buildDropdown(title: "Horário", value: horarioSelec, items: horarios, onChanged: (value) {
-                setState(() {
-                  horarioSelec = value;
-                });
-              }),
-              buildDropdown(title: "Período", value: periodoSelec, items: periodos, onChanged: (value) {
-                setState(() {
-                  periodoSelec = value;
-                });
-              }),
-              buildDropdown(title: "Intervalo", value: intervaloSelec, items: intervalos, onChanged: (value) {
-                setState(() {
-                  intervaloSelec = value;
-                });
-              }),
-            ] else ...[
-              const CircularProgressIndicator(),
-            ],
-            Spacer(),
-            Container(
-              height: 50,
-              color: Colors.grey,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (context) => const RemediosPage()),
-                        (Route<dynamic> route) => false,
-                      );
-                    },
-                    child: const Text(
-                      "Cancelar",
-                      style: TextStyle(color: Colors.white, fontSize: 25),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: searchController,
+                      decoration: const InputDecoration(
+                        labelText: 'Pesquisar Medicamento',
+                        suffixIcon: Icon(Icons.search),
+                      ),
                     ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (context) => const RemediosPage()),
-                        (Route<dynamic> route) => false,
-                      );
-                    },
-                    child: const Text(
-                      "Salvar",
-                      style: TextStyle(color: Colors.white, fontSize: 25)
+                    DropdownButton<String>(
+                      value: medicamentoSelec,
+                      hint: const Text("Selecione um Medicamento"),
+                      isExpanded: true,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          medicamentoSelec = newValue;
+                        });
+                      },
+                      items: filteredNomeMedicamento.map<DropdownMenuItem<String>>((String name) {
+                        return DropdownMenuItem<String>(
+                          value: name,
+                          child: Text(name),
+                        );
+                      }).toList(),
                     ),
-                  ),
-                ],
+                    buildDropdown("Horário", selectedTime, times),
+                    buildDropdown("Período", selectedPeriod, periods),
+                    buildDropdown("Intervalo", selectedInterval, intervals),
+                  ],
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+          Container(
+            height: 50,
+            color: Colors.grey,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => const RemediosPage()),
+                      (Route<dynamic> route) => false,
+                    );
+                  },
+                  child: const Text(
+                    "Cancelar",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Implement save functionality or other actions here
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => const RemediosPage()),
+                      (Route<dynamic> route) => false,
+                    );
+                  },
+                  child: const Text("Salvar",
+                      style: TextStyle(color: Colors.white, fontSize: 25)),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  Widget buildDropdown({required String title, String? value, required List<String> items, required void Function(String?) onChanged}) {
+  Widget buildDropdown(String title, String? value, List<String> options) {
     return ListTile(
       title: DropdownButton<String>(
         value: value,
         hint: Text(title),
-        onChanged: onChanged,
-        items: items.map<DropdownMenuItem<String>>((String value) {
+        isExpanded: true,
+        onChanged: (String? newValue) {
+          setState(() {
+            switch (title) {
+              case "Horário":
+                selectedTime = newValue;
+                break;
+              case "Período":
+                selectedPeriod = newValue;
+                break;
+              case "Intervalo":
+                selectedInterval = newValue;
+                break;
+              default:
+            }
+          });
+        },
+        items: options.map<DropdownMenuItem<String>>((String value) {
           return DropdownMenuItem<String>(
             value: value,
             child: Text(value),
