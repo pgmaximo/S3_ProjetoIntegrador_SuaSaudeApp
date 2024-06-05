929672d906c19289eac066eeb022b0b3c2193462
Brunno <129430164+BrunnoS1@users.noreply.github.com>
2024-06-05 13:39:05 -0300

    testes tdd

--- test_driver/tdd.dart
+++ test_driver/tdd.dart
@@ -1,8 +1,5 @@
-import 'package:flutter_driver/driver_extension.dart';
 import 'package:teste_firebase/main.dart' as app;
 
 void main() {
-  enableFlutterDriverExtension();
-
   app.main();
 }
--- test_driver/tdd_test.dart
+++ test_driver/tdd_test.dart
@@ -1,49 +1,106 @@
+import 'dart:io';
+
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('App', () {
-    final userFieldFinder = find.byValueKey("userfield");
-    final passFieldFinder = find.byValueKey("passfield");
-    final loginButtonFinder = find.byValueKey("botaologin");
-    final homePageTextFinder = find.byValueKey("boasvindas");
-    final logoutButtonFinder = find.byValueKey("logout");
-
     FlutterDriver? driver;
-    driver = null;
 
     setUpAll(() async {
       driver = await FlutterDriver.connect();
+
+      //sleep pra esperar carregar firebase, hive etc
+      sleep(const Duration(seconds: 10));
     });
 
     test('inserir login e senha corretos', () async {
-      //deve passar
+      final userFieldFinder = find.byValueKey("userfield");
+      final passFieldFinder = find.byValueKey("passfield");
+      final loginButtonFinder = find.byValueKey("botaologin");
+      final homePageTextFinder = find.byValueKey("boasvindas");
       await driver!.tap(userFieldFinder);
       await driver!.enterText("test@gmail.com");
       await driver!.tap(passFieldFinder);
       await driver!.enterText("test123");
       await driver!.tap(loginButtonFinder);
-      await driver!
-          .waitFor(homePageTextFinder, timeout: const Duration(seconds: 5));
-
-      expect(await driver!.getText(homePageTextFinder),
-          "logado como: test@gmail.com");
-      await driver!.tap(logoutButtonFinder);
+      expect(await driver!.getText(homePageTextFinder), "Bem vindo test!");
     });
 
-    // test('inserir login e senha incorretos', () async {
-    //   //deve falhar
-    //   await driver!.tap(userFieldFinder);
-    //   await driver!.enterText("dsadnksal@gmail.com");
-    //   await driver!.tap(passFieldFinder);
-    //   await driver!.enterText("test123");
-    //   await driver!.tap(loginButtonFinder);
-    //   await driver!
-    //       .waitFor(homePageTextFinder, timeout: const Duration(seconds: 5));
+    test('adicionar pressao', () async {
+      //acha o botao de historico de pressao e aperta
+      final histPressaoButton = find.byValueKey("histPressaoButton");
+      await driver!.tap(histPressaoButton);
 
-    //   expect(await driver!.getText(homePageTextFinder),
-    //       "logado como: test@gmail.com");
-    // });
+      //acha o botao de adicionar pressao e aperta
+      final botaoAddPressao = find.byValueKey("botaoAddPressao");
+      await driver!.tap(botaoAddPressao);
+
+      //insere valor no campo de pressao do popup e clica em salvar
+      final addPressaoField = find.byValueKey("addPressaoField");
+      await driver!.tap(addPressaoField);
+      await driver!.enterText("9/6");
+      final salvarButton = find.byValueKey("salvarButton");
+      await driver!.tap(salvarButton);
+
+      // procura a entrada nova na lista
+      final newEntryFinder = find.byValueKey("pressaoListText_0");
+      expect(await driver!.getText(newEntryFinder), "Pressão: 9/6");
+
+      // voltar pra home
+      final appbarButton = find.byValueKey("appbarButton");
+      await driver!.tap(appbarButton);
+    });
+
+    test('adicionar glicemia', () async {
+      //acha o botao de historico de glicemia e aperta
+      final histGlicemiaButton = find.byValueKey("histGlicemiaButton");
+      await driver!.tap(histGlicemiaButton);
+
+      //acha o botao de adicionar glicemia e aperta
+      final botaoAddGlicemia = find.byValueKey("botaoAddGlicemia");
+      await driver!.tap(botaoAddGlicemia);
+
+      //insere valor no campo de glicemia do popup e clica em salvar
+      final addGlicemiaField = find.byValueKey("addGlicemiaField");
+      await driver!.tap(addGlicemiaField);
+      await driver!.enterText("83");
+      final salvarButton = find.byValueKey("salvarButton");
+      await driver!.tap(salvarButton);
+
+      // procura a entrada nova na lista
+      final newEntryFinder = find.byValueKey("glicemiaListText_0");
+      expect(await driver!.getText(newEntryFinder), "83mg/dL");
+
+      // voltar pra home
+      final appbarButton = find.byValueKey("appbarButton");
+      await driver!.tap(appbarButton);
+    });
+
+    test('adicionar peso', () async {
+      //acha o botao de historico de peso e aperta
+      final histPesoButton = find.byValueKey("histPesoButton");
+      await driver!.tap(histPesoButton);
+
+      //acha o botao de adicionar Peso e aperta
+      final botaoAddPeso = find.byValueKey("botaoAddPeso");
+      await driver!.tap(botaoAddPeso);
+
+      //insere valor no campo de Peso do popup e clica em salvar
+      final addPesoField = find.byValueKey("addPesoField");
+      await driver!.tap(addPesoField);
+      await driver!.enterText("80");
+      final salvarButton = find.byValueKey("salvarButton");
+      await driver!.tap(salvarButton);
+
+      // procura a entrada nova na lista
+      final newEntryFinder = find.byValueKey("pesoListText_0");
+      expect(await driver!.getText(newEntryFinder), "80.0kg");
+
+      // voltar pra home
+      final appbarButton = find.byValueKey("appbarButton");
+      await driver!.tap(appbarButton);
+    });
 
     tearDownAll(() async {
       if (driver != null) {
